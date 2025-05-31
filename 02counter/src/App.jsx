43ea89e7396ad00781473db531e79cf632c25aab
Jv <jaiveeryadav75033@gmail.com>
2025-06-01 03:18:32 +0530

    hooks(useState)

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter,setCounter] = useState(9);
+
+  const addValue = () => {
+    setCounter(counter+1);
+  }
+
+  const removeValue = () => {
+    setCounter(counter-1);
+  }
+
+
+  return (
+    <>
+     <h1>yo</h1>
+     <h2>Counter value: {counter}</h2>
+
+     <button onClick={addValue}>Add value</button>
+     <br />
+     <button onClick={removeValue}>Remove value</button>
+
+    </>
+  )
+}
+
+export default App
